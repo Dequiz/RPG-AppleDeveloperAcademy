40e0b9515e54ce6305cdd5973ac2979b8c77d43b
André Holovati <128110472+Dequiz@users.noreply.github.com>
2025-11-14 18:53:15 -0300

    Add files via upload

--- jogo.swift
+++ jogo.swift
@@ -1,3 +1,10 @@
+//
+//  Jogo.swift
+//  Jogo
+//
+//  Created by ANDRE HOLOVATI on 14/11/25.
+//
+
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
@@ -5,7 +12,7 @@
 func criarPersonagem() -> Heroi{
     print("Digite o seu nome aventureiro")
 let iniciarPersonagem = readLine() ?? ""
-let personagem = Heroi(nome: iniciarPersonagem, vida: 20,forca: 100000000,nivel: 1,xp: 0,vidaMaxima: 20)
+let personagem = Heroi(nome: iniciarPersonagem, vida: 20,forca: 10000,nivel: 1,xp: 0,vidaMaxima: 20)
 print("Seja bem vindo ao sonho \(personagem.nome)")
 return personagem
 }
@@ -14,23 +21,21 @@ func criarInimigo(nomeMostro:String, vidaMonstro: Int, forcaMonstro:Int, rangeXP
     return inimigo
 }
 
-func criarItem(nomeItem: String, preco: Int, buff:Int ) -> Item{
-    let item = Item(nome: nomeItem, preco: preco, buff: buff)
+func criarItem(id: Int, nomeItem: String, preco: Int, buff:Int ) -> Item{
+    let item = Item(id: id, nome: nomeItem, preco: preco, buff: buff)
     return item
 }
-func criarLoja(setArmazem: Set<Item>) -> Loja{
+func criarLoja(setArmazem: [Item]) -> Loja{
     let loja = Loja(armazem: setArmazem)
     return loja
-}
-
+}   
 func comprarItem(loja : Loja, personagem : Heroi) -> Item{
     let itens = loja.armazem
-
     print("Digite o número do item que você deseja comprar")
     if let entrada = readLine(), let escolha = Int(entrada), escolha > 0, escolha <= itens.count{
-        // let itemEscolhido = itens[escolha - 1]
+       return itens[escolha - 1]
     }
-    
+    return itens[0]
 }
 
 func combate(protagonista: Heroi, inimigo: Inimigo){
@@ -58,7 +63,8 @@ while protagonista.vida > 0 && inimigo.vida > 0{
 if(protagonista.vida <= 0){
     print("\(protagonista.nome) morreu no nível \(protagonista.nivel)")
 }else{
-    print("\(protagonista.nome) derrotou o \(inimigo.nome) e ganhou \(inimigo.exp) de XP")
+    print("\(protagonista.nome) derrotou o \(inimigo.nome) e ganhou \(inimigo.exp) de XP\n Dinheiro dropado por \(inimigo.nome) : \(inimigo.dinheiroDropado)")
+    protagonista.dinheiro += inimigo.dinheiroDropado
     protagonista.xp += inimigo.exp
     protagonista.subirNivel()
 }
@@ -75,7 +81,7 @@ while protagonista.vida > 0 && inimigo.vida > 0{
 
     print("Vida atual de  \(inimigo.nome): \(inimigo.vida)")
     if inimigo.vida <= 0 {
-        return true
+        break
     }
 
      print("\(inimigo.nome) atacou causando \(inimigo.forca) de dano")
@@ -90,11 +96,25 @@ if(protagonista.vida <= 0){
     print("\(protagonista.nome) morreu no nível \(protagonista.nivel)")
     return false
 }else{
-    print("\(protagonista.nome) derrotou o \(inimigo.nome) e ganhou \(inimigo.exp) de XP")
+    print("\(protagonista.nome) derrotou o \(inimigo.nome) e ganhou \(inimigo.exp) de XP\n Dinheiro dropado por \(inimigo.nome) : \(inimigo.dinheiroDropado)")
+    protagonista.dinheiro += inimigo.dinheiroDropado
     protagonista.xp += inimigo.exp
     protagonista.subirNivel()
 }
 return protagonista.vida > 0
+}
+
+
+func compraVeridica(protagonista : Heroi, x: Item) -> Bool{
+    if(protagonista.dinheiro >= x.preco){
+        print("Voce comprou \(x.nome)")
+        protagonista.dinheiro -= x.preco
+        protagonista.inventario.append(x)
+        return true
+    }else{
+        print("Voce nao tem dinheiro suficiente")
+        return false
+    }
 }
 
 
